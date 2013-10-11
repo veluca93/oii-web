0ceaa008cfc9841e6906c8a34f81a7f60a41290a
Luca Versari <veluca93@gmail.com>
2013-10-11 09:57:42 +0200

    Added some debug messages to PWS

--- cms/server/PracticeWebServer.py
+++ cms/server/PracticeWebServer.py
@@ -127,10 +127,12 @@ class APIHandler(object):
 
     def load_json(self, request):
         if request.mimetype != "application/json":
+            logger.warning("Request not in JSON")
             raise BadRequest()
         try:
             req = json.load(request.stream)
         except (ValueError, TypeError):
+            logger.warning("JSON parse error")
             raise BadRequest()
         return req
 
@@ -226,6 +228,7 @@ class APIHandler(object):
             rtype = req["type"]
             rvalue = req["value"]
         except KeyError:
+            logger.warning("Missing parameters")
             raise BadRequest()
 
         if rtype == "username":
@@ -233,6 +236,7 @@ class APIHandler(object):
         elif rtype == "email":
             return self.dump_json(self.check_email(rvalue))
 
+        logger.warning("Request type not understood")
         raise BadRequest()
 
     def register_handler(self, request):
@@ -244,6 +248,7 @@ class APIHandler(object):
             firstname = req["firstname"]
             lastname = req["lastname"]
         except KeyError:
+            logger.warning("Missing parameters")
             raise BadRequest()
 
         sha = hashlib.sha256()
@@ -282,6 +287,7 @@ class APIHandler(object):
             username = req["username"]
             password = req["password"]
         except KeyError:
+            logger.warning("Missing parameter")
             raise BadRequest()
 
         sha = hashlib.sha256()
@@ -329,6 +335,7 @@ class APIHandler(object):
         try:
             last = datetime.utcfromtimestamp(req["last"])
         except (ValueError, KeyError):
+            logger.warning("Datetime not understood")
             raise BadRequest()
 
         resp = dict()
@@ -347,7 +354,7 @@ class APIHandler(object):
                 result = s.get_result()
                 for i in ["compilation_outcome", "evaluation_outcome",
                           "score"]:
-                    submission[i] = getattr(result, i)
+                    submission[i] = getattr(result, i, None)
                 submissions.append(submission)
         resp["submissions"] = submissions
         return self.dump_json(resp)
@@ -370,7 +377,7 @@ class APIHandler(object):
             for i in ["compilation_outcome", "evaluation_outcome",
                       "score", "compilation_stdout", "compilation_stderr",
                       "compilation_time", "compilation_memory"]:
-                submission[i] = getattr(result, i)
+                submission[i] = getattr(result, i, None)
             if result.score_details is not None:
                 submission["score_details"] = json.loads(result.score_details)
             else:
