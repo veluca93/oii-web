6f8cdfdb4d9bfd994faa3d63a5c953e7d17e9b58
William Di Luigi <williamdiluigi@gmail.com>
2013-10-19 18:09:19 +0200

    Refactoring di tutto, ora usiamo ui-router
    inoltre ho eliminato il filtro "capitalize" che ora facciamo con una regola css

--- cms/web/practice/js/tasks.js
+++ cms/web/practice/js/tasks.js
@@ -147,9 +147,9 @@ angular.module('pws.tasks', [])
     return this;
   }])
   .controller('TasksCtrl', [
-      '$scope', '$routeParams', '$location', '$http', '$window', 'notificationHub',
-      function($scope, $routeParams, $location, $http, $window, hub) {
-    $scope.startIndex = parseInt($routeParams.startIndex);
+      '$scope', '$stateParams', '$location', '$http', '$window', 'notificationHub',
+      function($scope, $stateParams, $location, $http, $window, hub) {
+    $scope.startIndex = parseInt($stateParams.startIndex);
     $scope.tasksPerPage = 5;
     $scope.$window = $window;
     $scope.updPage = function(newIndex) {
@@ -165,78 +165,4 @@ angular.module('pws.tasks', [])
       }).error(function(data, status, headers, config) {
         hub.createAlert('danger', 'Errore di connessione', 2);
     });
-  }])
-  .controller('TaskpageCtrl', [
-      '$scope', '$routeParams', '$location', '$http', '$window', '$rootScope', 'userManager', 'notificationHub', 'subsDatabase',
-      function($scope, $routeParams, $location, $http, $window, $rootScope, user, hub, subs) {
-    $("#timeLimit").popover();
-    $("#memoLimit").popover();
-    $scope.isLogged = user.isLogged;
-    $scope.taskName = $routeParams.taskName;
-    $scope.setActiveTab = function(tab) {
-        $location.path("/" + tab + "/" + $scope.taskName);
-    };
-    $scope.isActiveTab = function(tab) {
-      return $location.path().indexOf(tab) == 1;
-    };
-    $scope.$window = $window;
-    $http.post('task/' + $scope.taskName, {})
-      .success(function(data, status, headers, config) {
-        $scope.$window.task = data;
-      }).error(function(data, status, headers, config) {
-        hub.createAlert('danger', 'Errore di connessione', 2);
-    });
-    if (user.isLogged() && $scope.isActiveTab("submissions")) {
-      subs.load($scope.taskName);
-      $scope.loadFiles = function() {
-        var input = $("#submitform input");
-        $window.loadCount = input.length;
-        $window.files = {};
-        for (var i=0; i<input.length; i++) {
-          if (input[i].files.length < 1) {
-            hub.createAlert('danger', 'Files mancanti!', 2);
-            break;
-          }
-          var reader = new FileReader();
-          reader.readAsBinaryString(input[i].files[0]);
-          reader.filename = input[i].files[0].name
-          reader.inputname = input[i].name
-          reader.onloadend = function(){
-            $window.loadCount -= 1;
-            $window.files[reader.inputname] = {
-                "filename": reader.filename,
-                "data": reader.result,
-            };
-            if ($window.loadCount == 0)
-              $scope.submitFiles();
-          }
-        }
-        $("#submitform").each(function() {
-          this.reset();
-        });
-      }
-      $scope.submitFiles = function() {
-        var data = {};
-        data["username"] = user.getUsername();
-        data["token"] = user.getToken();
-        data["files"] = $window.files;
-        delete $window.files;
-        delete $window.loadCount;
-        $http.post('submit/' + $scope.taskName, data)
-          .success(function(data, status, headers, config) {
-            if (data["success"])
-              subs.addSub($scope.taskName, data);
-            else
-              hub.createAlert('danger', data["error"], 2);
-        }).error(function(data, status, headers, config) {
-            hub.createAlert('danger', 'Errore di connessione', 2);
-        });
-      }
-      $scope.showDetails = function(id) {
-        if ($rootScope.curSub == id)
-          $rootScope.curSub = 0;
-        else
-          subs.subDetails(id);
-      }
-    }
   }]);
